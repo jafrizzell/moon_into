ba845dbcdb80ba5afa7c3c44b150d75f62f6a8d7
jafrizzell <69974492+jafrizzell@users.noreply.github.com>
2021-07-11 13:40:36 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -29,8 +29,8 @@ function resize() {
 }
 resize();
 
-var baseImage = new Image();
-baseImage.src = 'https://github.com/jafrizzell/moon_intro/blob/main/better_moon2COFFEE.png?raw=true';
+var baseImage = new Image(810, 1080);
+baseImage.src = 'https://github.com/jafrizzell/moon_intro/blob/main/better_moon2COFFEE.PNG?raw=true';
 baseImage.onload = function() {
    var imageWidth = this.offsetWidth,
       imageHeight = this.offsetHeight;
