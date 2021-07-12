23ee6bac599c35937a23d9a85f2b4640a74cdbed
jafrizzell <69974492+jafrizzell@users.noreply.github.com>
2021-07-12 11:55:27 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -28,12 +28,26 @@ function resize() {
 resize();
 window.addEventListener('resize', resize);
 
-const ctx = canvas.getContext('2d');
-const image = document.getElementById('https://raw.githubusercontent.com/jafrizzell/moon_intro/main/deadlole.JPG');
+const image = new Image(); // Using optional size for image
+image.onload = drawImageActualSize; // Draw when image has loaded
 
-image.addEventListener('load', e => {
-  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-});
+// Load an image of intrinsic size 300x227 in CSS pixels
+image.src = 'https://raw.githubusercontent.com/jafrizzell/moon_intro/main/deadlole.JPG';
+
+function drawImageActualSize() {
+  // Use the intrinsic size of image in CSS pixels for the canvas element
+  canvas.width = this.naturalWidth;
+  canvas.height = this.naturalHeight;
+
+  // Will draw the image as 300x227, ignoring the custom size of 60x45
+  // given in the constructor
+  ctx.drawImage(this, 0, 0);
+
+  // To use the custom size we'll have to specify the scale parameters
+  // using the element's width and height properties - lets draw one
+  // on top in the corner:
+  ctx.drawImage(this, 0, 0, this.width, this.height);
+}
 
 
 
