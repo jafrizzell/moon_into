0361b805eb40a353fc925b9577526a7beadabdc2
jafrizzell <69974492+jafrizzell@users.noreply.github.com>
2021-07-13 20:34:08 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -71,29 +71,11 @@ function draw() {
 
 // add a callback function for when a new message with emotes is sent
 const emoteArray = [];
+let prevTime = Date.now();
 ChatInstance.on("emotes", (emotes) => {
-    if (emoteArray.length = 0) {
-        const type = Math.floor(Math.random() * 100);
-        var yVal = 0;
-        if (type < 70) {
-            const obs = 0;
-            yVal = canvas.height * 0.5;
-        }
+        if (Date.now() - prevTime < 1250) {}
         else {
-            const obs = 1;
-            yVal = canvas.height * 0.6;
-        }
-        emoteArray.push({
-            emotes,
-            x: canvas.width * 0.7,
-            y: yVal,
-            obstacle: 0,
-            spawn: Date.now()
-        })
-    }
-    else {
-        if (Date.now() - emoteArray[-1].emotes.spawn < 1250) {}
-        else {
+            prevTime = Date.now();
             const type = Math.floor(Math.random() * 100);
             var yVal = 0;
             if (type < 70) {
@@ -111,8 +93,8 @@ ChatInstance.on("emotes", (emotes) => {
                 obstacle: 0,
                 spawn: Date.now()
             })
+            
         }
-    }
 })
 
 draw();
